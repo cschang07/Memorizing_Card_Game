21080eaffb6a1cf4b828f9625de732f6082ea94c
cschang07 <manutdsfan@gmail.com>
2021-08-29 15:22:10 +0800

    first time

---
+++ index.js
@@ -0,0 +1,209 @@
+const GAME_STATE = {
+  FirstCardAwaits: 'FirstCardAwaits',
+  SecondCardAwaits: 'SecondCardAwaits',
+  CardsMatchFailed: 'CardsMatchFailed',
+  CardsMatched: 'CardsMatched',
+  GameFinished: 'GameFinished'
+}
+
+const Symbols = [
+  'https://image.flaticon.com/icons/svg/105/105223.svg', // 黑桃
+  'https://image.flaticon.com/icons/svg/105/105220.svg', // 愛心
+  'https://image.flaticon.com/icons/svg/105/105212.svg', // 方塊
+  'https://image.flaticon.com/icons/svg/105/105219.svg' // 梅花
+]
+
+//The 1ST time applying the MVC principle
+const view = {
+  //處理正面元素
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+
+    return `        <p>${number}</p>
+        <img src="${symbol}" />
+        <p>${number}</p>`
+  },
+
+  //處理牌背元素
+  getCardElement(index) {
+    return `
+      <div data-index="${index}" class="card back">
+      </div>`
+  },
+
+  //轉換A,J,Q,K
+  transformNumber(number) {
+    switch (number) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return number
+    }
+  },
+
+  //原本的寫法displayCards: function displayCards() { ...  }
+  displayCards(indexes) {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join("")
+  },
+
+  //...會把我們的參數變成陣列，e.g. cards = [1,2,3,4,5]
+  flipCards(...cards) {
+
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        //如果是背面
+        //回傳正面
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index))
+        return
+      }
+      //如果是正面
+      //回傳背面
+      card.classList.add('back')
+      card.innerHTML = null
+    })
+  },
+
+  pairCard(...cards) {
+    cards.map(card => {
+      card.classList.add('paired')
+    })
+  },
+
+  renderScore(score) {
+    document.querySelector('.score').textContent = `score: ${score}`
+  },
+
+  renderTriedTimes(times) {
+    document.querySelector('.tried').textContent = `You've tried ${times} times`
+  },
+
+  appendWrongAnimation(...cards) {
+    cards.map(card => {
+      card.classList.add('wrong')
+      // CSS動畫只能播一次，畢竟.wrong已經加上去了。所以用下面的事件監聽器處理
+      card.addEventListener('animationend', event => {
+        event.target.classList.remove('wrong')
+      }, {
+        once: true
+      })
+
+    })
+  },
+
+  showGameFinished() {
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete! But it it worth it?</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.triedTimes} times</p>
+    `
+    const header = document.querySelector('#header')
+    header.before(div)
+  }
+}
+
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52))
+  },
+
+  //依照不同的遊戲狀態，做不同的行為
+  dispatchCardAction(card) {
+    if (!card.classList.contains('back')) {
+      return
+    }
+
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.SecondCardAwaits
+        break
+      case GAME_STATE.SecondCardAwaits:
+
+        view.renderTriedTimes(++model.triedTimes)
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        // 判斷配對是否成功, not yet done/////////////////////
+        if (model.isRevealedCardsMatched()) {
+          //配對正確
+          view.renderScore(model.score += 10)
+          this.currentState = GAME_STATE.CardsMatched
+          view.pairCard(...model.revealedCards)
+          model.revealedCards = []
+          if (model.score === 260) {
+            console.log('showGameFinished')
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()  // 加在這裡
+            return
+          }
+          this.currentState = GAME_STATE.FirstCardAwaits
+        } else {
+          //配對失敗
+          this.currentState = GAME_STATE.CardsMatchFailed
+          //要讓玩家有記憶的時間
+          view.appendWrongAnimation(...model.revealedCards)
+          setTimeout(this.resetCards, 1000) //設定一秒鐘的暫停
+
+        }
+        break
+    }
+
+    console.log('current state:', this.currentState)
+    console.log('revealed cards', model.revealedCards)
+  },
+
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
+    controller.currentState = GAME_STATE.FirstCardAwaits
+  }
+}
+
+
+const model = {
+  revealedCards: [],
+
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  },
+
+  score: 0,
+
+  triedTimes: 0
+}
+
+//not view, not model, not control... just a little tool
+const utility = {
+  //randomly assign 52 numbers, need argument 'count'
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1))
+        ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
+    }
+    return number
+  }
+}
+
+controller.generateCards()
+
+//Node List (array-like, not a real array. .map() cannot be applied here)
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    // view.appendWrongAnimation(card)
+    controller.dispatchCardAction(card)
+  })
+})
